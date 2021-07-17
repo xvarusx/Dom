b7e6ef6d531ec3c962b6352891d12acd8ac79f5b
oussema <harrabi.oussema6@hotmail.fr>
2021-07-17 23:44:34 +0100

    bug fi remove button

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 var removeBotton = document.getElementsByClassName('material-icons rm md-48')
 console.log(removeBotton);
-for (var i=0; i<removeBotton.length; i++)
+for (var i=0 ;i<removeBotton.length ; i++)
     {
-        var button=removeBotton[i];
+       
         
-            button.addEventListener('click',function(e){
+        removeBotton[i].addEventListener('click',function(e){
                 console.log('clicked');
                var buttonClicked=e.target;
               
                buttonClicked.parentElement.parentElement.remove();
             }) 
-    }
+}
+var likeButton = document.getElementsByClassName('material-icons recemendet');
+console.log(likeButton);
+for(var i=0;i<likeButton.length;i++){
+        var buttonlike=likeButton[i];
+        button.addEventListener('click',function(e){
+            var buttonLiked=e.target
+            buttonLiked.style.color='red';
+
+        })
+}
