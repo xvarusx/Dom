941bab6616bf0d2a84d50ebfc20a505f230bbd5c
oussema <harrabi.oussema6@hotmail.fr>
2021-07-18 18:20:27 +0100

    add total function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 var removeBotton = document.getElementsByClassName('delete')
-console.log(removeBotton);
 for (var i=0 ;i<removeBotton.length ; i++){
       var bottonRemove= removeBotton[i];
       bottonRemove.addEventListener('click',function(e){
-                console.log('clicked');
                var buttonClicked=e.target;
               
                buttonClicked.parentElement.parentElement.parentElement.remove();
-            }) 
+               updateCartTotal() ;
+            })
+           
 }
 var likeBotton = document.getElementsByClassName('like');
 for(var i=0;i<likeBotton.length;i++){
@@ -24,26 +24,41 @@ for(var i=0;i<likeBotton.length;i++){
 }
 var addBotton=document.getElementsByClassName('material-icons add')
 for(var i=0;i<addBotton.length;i++){
-    console.log(addBotton[i]);
     var bottonAdd=addBotton[i];
     bottonAdd.addEventListener('click',function(e){
     var botton=e.target;
     
    var inputField=botton.parentElement.firstElementChild.nextElementSibling;
-       inputField.value=parseInt(parseInt(inputField.value)+1);   
+       inputField.value=parseInt(parseInt(inputField.value)+1); 
+       updateCartTotal() ; 
 })
 }
 var decBotton=document.getElementsByClassName('material-icons rm')
 for(var i=0;i<decBotton.length;i++){
-    console.log(decBotton[i]);
     var bottonDec=decBotton[i];
     bottonDec.addEventListener('click',function(e){
     var botton=e.target;
     
    var inputField=botton.parentElement.firstElementChild.nextElementSibling;
-       inputField.value=parseInt(parseInt(inputField.value)-1);   
+       inputField.value=parseInt(parseInt(inputField.value)-1); 
+       updateCartTotal();  
 })
 }
-function updateCarttotal(){
+function updateCartTotal(){
+    var CartItemContainer=document.getElementsByClassName('cart__container')[0];
+    console.log(CartItemContainer);
+    var CartRows=CartItemContainer.getElementsByClassName('product-1');
+    console.log(CartRows);
+    total=0;
+    for(var i=0;i<CartRows.length;i++){
+        var cartRow = CartRows[i];
+        console.log(cartRow);
+        var cartPrice=cartRow.getElementsByClassName('product1__price')[0].firstElementChild;
+        var quantityElement=cartRow.getElementsByClassName('quantity')[0];
+        var price=parseFloat(cartPrice.innerHTML.replace('$',''));
+        var quantity = quantityElement.value;
+        var total =total+( price * quantity);
+    }
+    document.getElementsByClassName('total-value')[0].innerHTML='$'+total;
   
 }
